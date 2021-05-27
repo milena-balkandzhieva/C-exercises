ec39687e713e94d45e0ae029d3cda2c6aa6d5a79
milena-balkandzhieva <83964271+milena-balkandzhieva@users.noreply.github.com>
2021-05-27 09:34:26 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main(void) {
+	char name[50];
+	printf("Please enter a name:");
+	scanf("%s", name);
+	printf("Hello %s", name);
+}
